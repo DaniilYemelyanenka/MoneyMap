592ca4e5a65462310a573c4e3f1980674f068451
DaniilYemelyanenka <whitesmurf0612@gmail.com>
2025-05-20 18:49:37 +0300

    create user enum

--- src/main/java/by/yemelyanenko/MoneyMap/entity/User.java
+++ src/main/java/by/yemelyanenko/MoneyMap/entity/User.java
@@ -1,4 +1,33 @@
 package by.yemelyanenko.MoneyMap.entity;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "mm_user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String email;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "second_name")
+    private String secondName;
+
+    private String role;
+
+    private LocalDate createdAt;
+
+    private boolean isActive;
+
+
 }
